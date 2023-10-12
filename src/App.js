aaea5f7fb4615c4af77fbec4b97a9eebd53aed4d
Asha Gaire <asha.gaire95@gmail.com>
2023-10-12 12:52:40 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './style.css';
+import Main from './components/Main'
+import Navbar from './components/Navbar'
+
+// function Footer() {
+//   return(
+//     <footer> @2023 Asha Development. All rights reserved</footer>
+//   )
+// }
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navbar />
+        <Main />
+        
+        
+        
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
